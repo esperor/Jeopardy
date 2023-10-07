d4538c33f167501d63e5454ff432e82aae65cf06
twink000 <147235423+twink000@users.noreply.github.com>
2023-10-07 13:47:27 +0500

    fix: missing }

--- src/app.tsx
+++ src/app.tsx
@@ -119,7 +119,7 @@ class Main extends React.Component {
     return (
       <div>
         <div className="centered hbox pop-down" id="pop-down"></div>
-        <Header onBtnInputJSONClick={() => this.onBtnInputJSONClick() 
+        <Header onBtnInputJSONClick={() => this.onBtnInputJSONClick()}
         onBtnInputDIRClick={() => this.onBtnInputDIRClick()} onBtnNextRoundClick={() => this.onBtnNextRoundClick()} onBtnPreviousRoundClick={() => this.onBtnPreviousRoundClick()}}>
         </Header>
         <h1 className="centered" id="pregame-header"></h1>
